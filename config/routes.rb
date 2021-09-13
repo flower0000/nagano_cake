324b99e7127c3ba9051e538b774bcf83084dce10
flower0000 <knaoki81@gmail.com>
2021-09-13 22:44:40 +0000

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #homesコントローラー用
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  
+  
+  
 end
