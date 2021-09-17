ef7f834c41f698eed682eb626f8734e873ffa85d
flower0000 <knaoki81@gmail.com>
2021-09-17 05:59:42 +0000

    商品新規登録画面作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,47 @@
 class Admin::ItemsController < ApplicationController
+
+  def index
+
+  end
+
+
+
+  def new
+    @item = Item.new
+  end
+
+
+
+
+  def create
+    @item = Item.new(item_params) #追加した商品情報をインスタンス変数に入れる
+    @item.save #情報をセーブ
+    redirect_to item_path(@item.id) #商品詳細ページに遷移
+  end
+
+
+
+
+  def show
+
+  end
+
+
+
+
+  def edit
+
+  end
+
+
+
+
+  def update
+
+  end
+  
+  def item_params
+    params.require(:item).permit(:image_id, :name, :introduction, :price)
+  end
+  
 end
