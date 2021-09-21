c609a2a4fb57cfdcb69be701e7616933f9abab46
flower0000 <knaoki81@gmail.com>
2021-09-21 09:43:07 +0000

    ユーザー詳細画面作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,23 @@
 class CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+    #current_で現在ログインしている人の情報をさす(なにモデルでログインしているか)。今回はcustomerモデルを通しているのでcurrent_customerになる。
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def check
+
+  end
+
+  def withdrawal
+
+  end
+
 end
