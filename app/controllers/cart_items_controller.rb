2d06751054da204fd72250767e0fe1d775151242
flower0000 <knaoki81@gmail.com>
2021-09-26 09:24:08 +0000

    カート画面作成

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,6 +3,7 @@ class CartItemsController < ApplicationController
 
 
   def index
+
     @cart_items = current_customer.cart_items
     #1ユーザー(current_customer)のみの全cart_itemデータを取得する
     @total = 0
@@ -10,27 +11,56 @@ class CartItemsController < ApplicationController
   end
 
 
-  def update
+  def update#カート内商品データ更新(カート内の商品注文個数変更)
     cart_item = CartItem.find(params[:id])
-    cart_item.update
+    cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
 
 
   def destroy
+    cart_item = current_customer.cart_items.find(params[:id])
+    #現在ログインしているデータの全カートアイテムの中から該当するIDを持つデータを取得
 
+    cart_item.destroy
+    #該当データを削除
+
+    redirect_to cart_items_path
   end
 
 
   def destroy_all
-
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.save
-    redirect_to cart_items_path
+    @cart_item = CartItem.new(cart_item_params)
+    #既にあるデータと送られてきたデータを分けるためにこっちの変数に@を付けている．
+    #cart_itemはもともと保存してあったデータ側なので間違えずに使用
+
+    if current_customer.cart_items.find_by(item_id: @cart_item.item_id).present?
+      #.present?でデータの有無の確認をする．あればtrueでなければfalseを返す。
+
+      #current_customerのカートアイテムの全データ（モデル）をfindメソッドを使用して検索。
+      #find_by(item_id: @cart_item.item_id)→find_by(item_id: params[:cart_item][:items_id])
+      #飛ばされてきたデータ　params[:cart_item]。この中のitem_idを探す。
+
+      #ストロングパラメータ（フォームから送られてきた値）をデータを探すときにそのまま使用できる
+
+      cart_item = current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+      #ログインしているユーザーの全カートアイテム情報から，見つけたいデータ(item_id)を指定して、@cart_item.item_id(同じ商品id)を持つデータが無いか検索する．
+      #あればそのIDを持つ1レコード分のデータをcart_itemに入れている
+      cart_item.amount += @cart_item.amount.to_i
+
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   #ストロングパラメータの設定
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,10 @@ class Item < ApplicationRecord
    has_many :order_details, dependent: :destroy
    
    attachment :image #(image_idの_idは含めず入力)
+   
+   #消費税を求めるメソッド
+   def with_tax_price
+     (price * 1.1).floor
+   end
+   
 end
